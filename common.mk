cd872fd48eadc3e00d15fac15e5793cafef1bca7
Omkar Chandorkar <gotenksIN@aospa.co>
2024-02-10 23:03:03 +0900

    common: Switch to qssi device_framework_manifest
    - Reduces maintenance
    
    Change-Id: I4558fa90b0374037025dce75e56308d0b95228f6
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- common.mk
+++ common.mk
@@ -189,6 +189,9 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     vendor/qcom/opensource/core-utils/vendor_framework_compatibility_matrix.xml \
     vendor/aosp/config/device_framework_matrix.xml
 
+DEVICE_FRAMEWORK_MANIFEST_FILE += \
+    device/qcom/qssi/framework_manifest.xml
+
 PRODUCT_VENDOR_PROPERTIES += ro.vendor.qti.va_aosp.support=1
 PRODUCT_ODM_PROPERTIES += ro.vendor.qti.va_odm.support=1
 endif
